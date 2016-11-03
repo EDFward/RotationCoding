218a309e01c376b93700772f805d69d7918c2d38
Junjia He <edfward@gmail.com>
2016-11-03 00:02:04 -0700

    Add optimized coding algorithm and profiling

--- src/com/edfward/RotationCoding/Main.java
+++ src/com/edfward/RotationCoding/Main.java
@@ -1,93 +1,238 @@
 package com.edfward.RotationCoding;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.RandomStringUtils;
+interface RotationCoding {
+  String code(String s);
 
-public class Main {
-    public static void main(String[] args) {
-        String randomString = RandomStringUtils.randomAlphanumeric(1000000);
-
-        // Run 100 times.
-        for (int i = 0; i < 200; i++) {
-            String encoded = RotationCoding.code(randomString);
-            String decoded = RotationCoding.decode(encoded);
-
-            boolean same = randomString.equals(decoded);
-            System.out.println("Source and decoded are the same? " + (same ? "Yes." : "No"));
-        }
-    }
+  String decode(String s);
 }
 
-class RotationCoding {
+public class Main {
+  public static void main(String[] args) {
+    boolean shouldPrint = args.length > 0 && args[0].equals("print");
+    String randomString = RandomStringUtils.randomAlphanumeric(100000);
 
-    public static String code(String s) {
-        int size = s.length();
-        if (size == 0) {
-            return "";
+    RotationCoding coding = new RotationCodingOriginal();
+
+    // Run multiple times.
+    for (int i = 0; i < 5000; i++) {
+      String encoded = coding.code(randomString);
+      String decoded = coding.decode(encoded);
+
+      if (shouldPrint) {
+        boolean same = randomString.equals(decoded);
+        System.out.println("Source and decoded are the same? " + (same ? "Yes." : "No"));
+      }
+    }
+  }
+}
+
+class RotationCodingOriginal implements RotationCoding {
+
+  private static char[][] rotate(char[][] src, boolean clockwise) {
+    int srcSize = src.length;
+    char[][] res = new char[srcSize][srcSize];
+    if (clockwise) {  // Clockwise.
+      for (int row = 0; row < srcSize; row++) {
+        for (int col = 0; col < srcSize; col++) {
+          res[col][srcSize - 1 - row] = src[row][col];
         }
-
-        int n = ((int) Math.sqrt(size - 1)) + 1;
-        char[][] chs = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int chIndex = i * n + j;
-                chs[i][j] = chIndex >= size ? 11 : s.charAt(chIndex);
-            }
+      }
+    } else {  // Counter-clockwise.
+      for (int row = 0; row < srcSize; row++) {
+        for (int col = 0; col < srcSize; col++) {
+          res[srcSize - 1 - col][row] = src[row][col];
         }
-        char[][] rotatedClockwise = rotate(chs, true);
+      }
+    }
+    return res;
+  }
 
-        return Arrays.stream(rotatedClockwise)
-            .map(String::new)
-            .collect(Collectors.joining("\n"));
+  public String code(String s) {
+    int size = s.length();
+    if (size == 0) {
+      return "";
     }
 
-    public static String decode(String s) {
-        s = s.replace("\n", "");
-        int size = s.length();
-        if (size == 0) {
-            return "";
-        }
+    int n = ((int) Math.sqrt(size - 1)) + 1;
+    char[][] chs = new char[n][n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        int chIndex = i * n + j;
+        chs[i][j] = chIndex >= size ? 11 : s.charAt(chIndex);
+      }
+    }
+    char[][] rotatedClockwise = rotate(chs, true);
 
-        int n = (int) Math.sqrt(size);
-        char[][] chs = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                chs[i][j] = s.charAt(i * n + j);
-            }
-        }
-        char[][] rotatedCounterClockwise = rotate(chs, false);
+    return Arrays.stream(rotatedClockwise)
+        .map(String::new)
+        .collect(Collectors.joining("\n"));
+  }
 
-        StringBuilder resBuilder = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                char ch = rotatedCounterClockwise[i][j];
-                if (ch == 11) {
-                    break;
-                }
-                resBuilder.append(ch);
-            }
-        }
-        return resBuilder.toString();
+  public String decode(String s) {
+    s = s.replace("\n", "");
+    int size = s.length();
+    if (size == 0) {
+      return "";
     }
 
-    private static char[][] rotate(char[][] src, boolean clockwise) {
-        int srcSize = src.length;
-        char[][] res = new char[srcSize][srcSize];
-        if (clockwise) {  // Clockwise.
-            for (int row = 0; row < srcSize; row++) {
-                for (int col = 0; col < srcSize; col++) {
-                    res[col][srcSize - 1 - row] = src[row][col];
-                }
-            }
-        } else {  // Counter-clockwise.
-            for (int row = 0; row < srcSize; row++) {
-                for (int col = 0; col < srcSize; col++) {
-                    res[srcSize - 1 - col][row] = src[row][col];
-                }
-            }
-        }
-        return res;
+    int n = (int) Math.sqrt(size);
+    char[][] chs = new char[n][n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        chs[i][j] = s.charAt(i * n + j);
+      }
     }
+    char[][] rotatedCounterClockwise = rotate(chs, false);
+
+    StringBuilder resBuilder = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        char ch = rotatedCounterClockwise[i][j];
+        if (ch == 11) {
+          break;
+        }
+        resBuilder.append(ch);
+      }
+    }
+    return resBuilder.toString();
+  }
+}
+
+class RotationCodingV1 implements RotationCoding {
+
+  private static char[][] rotate(String src, int n, boolean clockwise) {
+    int srcSize = src.length();
+    char[][] res = new char[n][n];
+    if (clockwise) {  // Clockwise.
+      for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+          int idx = row * n + col;
+          char ch = idx >= srcSize ? 11 : src.charAt(idx);
+          res[col][n - 1 - row] = ch;
+        }
+      }
+    } else {  // Counter-clockwise.
+      for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+          int idx = row * n + col;
+          char ch = idx >= srcSize ? 11 : src.charAt(idx);
+          res[n - 1 - col][row] = ch;
+        }
+      }
+    }
+    return res;
+  }
+
+  public String code(String s) {
+    int size = s.length();
+    if (size == 0) {
+      return "";
+    }
+
+    int n = ((int) Math.sqrt(size - 1)) + 1;
+    char[][] rotatedClockwise = rotate(s, n, true);
+
+    return Arrays.stream(rotatedClockwise)
+        .map(String::new)
+        .collect(Collectors.joining("\n"));
+  }
+
+  public String decode(String s) {
+    s = s.replace("\n", "");
+    int size = s.length();
+    if (size == 0) {
+      return "";
+    }
+
+    int n = (int) Math.sqrt(size);
+    char[][] rotatedCounterClockwise = rotate(s, n, false);
+
+    StringBuilder resBuilder = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        char ch = rotatedCounterClockwise[i][j];
+        if (ch == 11) {
+          break;
+        }
+        resBuilder.append(ch);
+      }
+    }
+    return resBuilder.toString();
+  }
+}
+
+class RotationCodingV2 implements RotationCoding {
+
+  private static char[][] rotate(String src, int n, boolean clockwise) {
+    int srcSize = src.length();
+    char[][] res = new char[n][n];
+    if (clockwise) {  // Clockwise.
+      for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+          int idx = row * n + col;
+          char ch = idx >= srcSize ? 11 : src.charAt(idx);
+          res[col][n - 1 - row] = ch;
+        }
+      }
+    } else {  // Counter-clockwise.
+      for (int row = 0; row < n; row++) {
+        for (int col = 0; col < n; col++) {
+          int idx = row * n + col;
+          char ch = idx >= srcSize ? 11 : src.charAt(idx);
+          res[n - 1 - col][row] = ch;
+        }
+      }
+    }
+    return res;
+  }
+
+  public String code(String s) {
+    int size = s.length();
+    if (size == 0) {
+      return "";
+    }
+
+    int n = ((int) Math.sqrt(size - 1)) + 1;
+    char[][] rotatedClockwise = rotate(s, n, true);
+
+    StringBuilder res = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        res.append(rotatedClockwise[i][j]);
+      }
+      if (i != n - 1) {
+        res.append('\n');
+      }
+    }
+
+    return res.toString();
+  }
+
+  public String decode(String s) {
+    s = s.replace("\n", "");
+    int size = s.length();
+    if (size == 0) {
+      return "";
+    }
+
+    int n = (int) Math.sqrt(size);
+    char[][] rotatedCounterClockwise = rotate(s, n, false);
+
+    StringBuilder resBuilder = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        char ch = rotatedCounterClockwise[i][j];
+        if (ch == 11) {
+          break;
+        }
+        resBuilder.append(ch);
+      }
+    }
+    return resBuilder.toString();
+  }
 }
